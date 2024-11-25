c553a0691bdc5609ed2ac8b056f12d84d504d621
tientran <tienco201@gmail.com>
2024-11-26 00:09:32 +0700

    feat: connected mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
-import usersRouter from './routes/users.routes'
+import usersRouter from '@/routes/users.routes'
+import databaseService from '@/services/database.services'
 
 const app = express()
 
@@ -7,6 +8,7 @@ const port = 3000
 
 app.use(express.json())
 app.use('/users', usersRouter)
+databaseService.connect()
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
